9fa2e950b7a330a1783a7cc54895f562753254c4
ahmetk5113 <ahmetk5113@hotmail.com>
2021-07-28 17:50:52 +0300

    added coffee get method to menu

--- routes/menu.js
+++ routes/menu.js
@@ -9,7 +9,7 @@ const coffees = [
     { name: "latte", price: 20, url: "https://scontent.fecn7-1.fna.fbcdn.net/v/t1.6435-9/216991470_101965968839118_7616420094217531027_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=730e14&_nc_ohc=grqzla36-HUAX_6bW32&_nc_ht=scontent.fecn7-1.fna&oh=81731d67ffdbbd2ec6a64dbef7d7fb08&oe=6125B079" },
     { name: "americano", price: 22, url: "https://scontent.fecn7-1.fna.fbcdn.net/v/t1.6435-9/217392436_101965985505783_3195504378779882822_n.jpg?_nc_cat=109&ccb=1-3&_nc_sid=730e14&_nc_ohc=UxSFPz7zc_AAX_VLWfZ&_nc_ht=scontent.fecn7-1.fna&oh=5c411a2a226aa746c15c09bd44fdfaea&oe=6123E2AA" }
 ]
-
+// search
 router.get("/search", tokenAuth.tokenAuthenticator, async (req, res) => {
     try {
         const filterByName = await coffees.filter(coffee => coffee.name.includes(req.body.name))
@@ -18,6 +18,8 @@ router.get("/search", tokenAuth.tokenAuthenticator, async (req, res) => {
         res.status(403).json({ message: err.message });
     }
 });
+
+// get token
 router.get('/token', sendToken.token,tokenAuth.tokenAuthenticator,  async (req, res) =>{
     try{
         const token = req.body.bearerHeader;
@@ -26,7 +28,17 @@ router.get('/token', sendToken.token,tokenAuth.tokenAuthenticator,  async (req, 
         return res.status(403).json({message : err.message})
     }
 });
+// get menu details
+router.get('/', async (req, res) =>{
+    try{
+        res.send({coffees})
+    }catch(err){
+        return res.status(403).json({message : err.message})
+    }
+})
 
+
+// buy
 router.put('/purchase/:id', tokenAuth.tokenAuthenticator, async (req, res) => {
     try {
         let credit;
